693f481dd21e041adb971179e78613fd110496c9
Marcelo Souza <mlesouza95@gmail.com>
2018-08-22 19:22:34 -0300

    Criado a interface das tabelas e bind dos dados

--- view/application/js/controller.js
+++ view/application/js/controller.js
@@ -2,18 +2,22 @@
     angular
         .module('app', [])
         .controller('XmlController'
-        , ['$scope', '$http', function($scope, $http){
+        ,['$scope', '$http', function($scope, $http){
 
       var viewModel = this;
-
+      viewModel.curso = {}
       
       $http.get('http://localhost:8079/curso').then(function(response) {
         viewModel.curso = response.data;
+        console.log(viewModel.curso);
       }, function(err) {
         console.log("Deu Ruim", err);
-      });            
+      });  
+      
+      console.log("entrou", viewModel.curso);
     }]);
 
 
+
 })();
-    
+
